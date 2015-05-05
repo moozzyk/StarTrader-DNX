6bb977283558f27be149699e5fe36cd551998433
Pawel Kadluczka <moozzyk@gmail.com>
2015-05-04 21:41:39 -0700

    Adding MVC application skeleton

---
+++ src/StarTrader/Startup.cs
@@ -0,0 +1,31 @@
+using Microsoft.AspNet.Builder;
+using Microsoft.AspNet.Hosting;
+using Microsoft.Framework.DependencyInjection;
+using Microsoft.Framework.Logging;
+
+namespace StarTrader
+{
+	public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc();
+        }
+
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+        {
+            loggerFactory.AddConsole();
+//             Add static files to the request pipeline.
+//             app.UseStaticFiles();
+
+            // Add MVC to the request pipeline.
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller}/{action}/{id?}",
+                    defaults: new { controller = "Home", action = "Index" });
+            });
+        }
+    }
+}
